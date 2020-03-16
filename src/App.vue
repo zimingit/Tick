25499d64e2cf8f28aced75c7e2a8cdd1b3423520
Aleksey <pyksik@gmail.com>
2020-03-17 00:50:42 +0300

    FEAT transition component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition :name="transitionName">
+      <router-view/>
+    </transition>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      transitionName: 'scale-in'
+    }
+  },
+  watch: {
+    $route (to, from) {
+      const toDepth = to.path.split('/').length
+      const fromDepth = from.path.split('/').length
+      this.transitionName = toDepth < fromDepth ? 'scale-out' : 'scale-in'
+    }
+  }
+}
+</script>
 
 <style lang="stylus">
 body, #app
@@ -15,4 +34,27 @@ body, #app
   flex 1
   height 100vh
   color #2c3e50
+  overflow hidden
+
+#app
+  overflow auto
+
+// Анимации переключения компонентов
+.scale-in-enter-active, .scale-in-leave-active
+.scale-out-enter-active, .scale-out-leave-active
+  position absolute
+  transform-origin top center
+  transition-property opacity, transform
+  transition-duration .4s
+  transition-timing-function ease-in-out
+
+.scale-in-enter,
+.scale-out-leave-active
+  opacity 0
+  transform scale(1.5)
+
+.scale-in-leave-active,
+.scale-out-enter
+  opacity 0
+  transform scale(.5)
 </style>
--- src/views/Tick.vue
+++ src/views/Tick.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    Tick
-    {{id}}
+  <div class="layout">
+    <div class="tick">
+      Tick
+    </div>
   </div>
 </template>
 
@@ -22,3 +23,23 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="stylus">
+.layout
+  display flex
+  flex 1
+  width 100%
+  height 100%
+
+.tick
+  width 900px
+  height 600px
+  margin auto
+  background #c8e6c9
+  display flex
+  justify-content center
+  align-items center
+  font-size 3em
+  font-weight 600
+  filter blur(3px)
+</style>
